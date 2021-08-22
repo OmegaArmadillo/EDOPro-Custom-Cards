4e894e237e7f15d2920c1f8e06ddeeb256601cf2
Matthew Hibner <matthewhibner3@gmail.com>
2021-08-21 23:49:22 -0700

    Changed effect
    fixed attack buff, added destroy by battle restriction, added effect restriction

--- script/c43000012.lua
+++ script/c43000012.lua
@@ -4,49 +4,54 @@ function s.initial_effect(c)
    
 	Fusion.AddProcMixN(c,true,true,s.ffilter,2) 
 
-	--atk up
+	--gain atk
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetOperation(s.sumcon)
-	e1:SetOperation(s.atkop)
+	e1:SetCondition(s.tgcon)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
 
-	--atk
+	--destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
+   -- e3:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(LOCATION_MZONE,0)
-	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x8))
-	e2:SetValue(500)
-	e2:SetCondition(s.tgcon)
+	e2:SetCountLimit(id)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
 
-	--destroy
+
 	local e3=Effect.CreateEffect(c)
-   -- e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DESTROY)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e3:SetType(EFFECT_TYPE_IGNITION)
+   --e4:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetTarget(s.destg)
-	e3:SetOperation(s.desop)
+	e3:SetCountLimit(id)
+	e3:SetCondition(s.bdescon)
+	e3:SetTarget(s.cdestg)
+	e3:SetOperation(s.cdesop)
+	e3:SetLabel(4)
 	c:RegisterEffect(e3)
 
 
+
+
+	--destroy by effect
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
-	--e4:SetCategory(CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+   --e4:SetDescription(aux.Stringid(id,1))
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_DESTROYED)
 	e4:SetRange(LOCATION_MZONE)
-	e4:SetCondition(s.adescon)
-	e4:SetTarget(s.adestg)
-	e4:SetOperation(s.adesop)
+	e4:SetCountLimit(id)
+	e4:SetCondition(s.edescon)
+	e4:SetTarget(s.cdestg)
+	e4:SetOperation(s.cdesop)
 	e4:SetLabel(4)
 	c:RegisterEffect(e4)
 
@@ -70,17 +75,42 @@ function s.tgcon(e)
 	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
 end
 
-function s.filter(c,atk)
-	return c:IsFaceup() and c:GetAttack()<atk
+
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x8)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
 end
 
 
+
+
+
 --destroy effect
+function s.tfilter(c,atk)
+	return c:IsFaceup() and c:GetAttack()<atk
+end
+
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc,e:GetHandler():GetAttack()) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e:GetHandler():GetAttack()) end
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) and s.tfilter(chkc,e:GetHandler():GetAttack()) end
+	if chk==0 then return Duel.IsExistingTarget(s.tfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e:GetHandler():GetAttack()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e:GetHandler():GetAttack())
+	local g=Duel.SelectTarget(tp,s.tfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e:GetHandler():GetAttack())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
@@ -104,9 +134,15 @@ end
 
 
 --choose effect when destroyed
-function s.adescon(e,tp,eg,ep,ev,re,r,rp)
+function s.edescon(e,tp,eg,ep,ev,re,r,rp)
 	return (r&REASON_EFFECT)~=0 and re:GetHandler()==e:GetHandler()
 end
+
+
+function s.bdescon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsRelateToBattle()
+end
+
 
 function s.serfilter(c)
 	return ((c:IsSetCard(0x8) and c:IsType(TYPE_MONSTER)) or c:IsCode(27847700,24094653)) and c:IsAbleToHand()
@@ -118,13 +154,13 @@ end
 
 
 
-function s.adestg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.cdestg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	if chk==0 then
 		--local ct=Duel.GetMatchingGroupCount(s.ctfilter,tp,LOCATION_MZONE,0,c)
 		local sel=0
 		if  Duel.IsExistingMatchingCard(s.serfilter,tp,LOCATION_DECK,0,1,1,nil) then sel=sel+1 end
-		if Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_DECK,0,1,nil) then sel=sel+2 end
+		if Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND,0,1,nil) then sel=sel+2 end
 		e:SetLabel(sel)
 		return sel~=0
 	end
@@ -142,10 +178,10 @@ function s.adestg(e,tp,eg,ep,ev,re,r,rp,chk)
 		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_MZONE)
 	else
 		e:SetCategory(CATEGORY_SPECIAL_SUMMON)
-		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+		Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 	end
 end
-function s.adesop(e,tp,eg,ep,ev,re,r,rp)
+function s.cdesop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local sel=e:GetLabel()
 	if sel==1 then
@@ -157,9 +193,20 @@ function s.adesop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	else
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()   
-	Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)		
+ 
+		if tc and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+			--Cannot activate its effects this turn
+			local e1=Effect.CreateEffect(c)
+			e1:SetDescription(3302)
+			e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_CANNOT_TRIGGER)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+
 	Duel.SpecialSummonComplete()
 	end
 end
